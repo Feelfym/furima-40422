3aad1c11eaf5e2e7e009eca38b4efc789208cfbc
Feelfym <tomoya.noguchi@gmail.com>
2024-03-24 19:23:39 +0900

    新規登録・ログイン機能
    バリデーションの設定含む

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to root_path
+    else
+      render :new, :unprocessable_entity
+    end
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+    validates :last_name,  format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "全角ひらがな、全角カタカナ、漢字で入力して下さい" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
+    validates :last_name_kana,  format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
+    validates :birthday
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードには英字と数字の両方を含めてください' 
+
+end
