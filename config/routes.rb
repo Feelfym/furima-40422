13dfa6ce059a35399b2d5c3f421c6fb3ce43d418
Feelfym <tomoya.noguchi@gmail.com>
2024-04-05 20:44:37 +0900

    オーダー画面への遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'  
-  resources :items
+  resources :users, only: [:new, :create]
+  resources :items do
+    resources :orders, only: [:index, :new, :create]
+  end
 end
