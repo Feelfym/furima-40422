0c732cf278249a2c6eac73e0e6a74871d2c38b2a
Feelfym <tomoya.noguchi@gmail.com>
2024-04-07 17:29:24 +0900

    エラーメッセージ設定、テストコード作成

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -11,14 +11,16 @@ class OrderAddress
                 :token
 
   with_options presence: true do
-    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'ハイフンを含めた半角数字で入力してください' }
-    validates :prefecture_id, numericality: { other_than: 1, message: 'を選択してください' }
-    validates :city
-    validates :house_number
-    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: '半角数字で入力してください' }
-    validates :token
-    validates :user_id
-    validates :item_id
+    validates :postal_code, presence: { message: '郵便番号を入力してください' },
+                            format: { with: /\A\d{3}[-]\d{4}\z/, message: '郵便番号はハイフンを含めた半角数字で入力してください' }
+    validates :prefecture_id, numericality: { other_than: 1, message: '都道府県を選択してください' }
+    validates :city, presence: { message: '市区町村を入力してください' }
+    validates :house_number, presence: { message: '番地を入力してください' }
+    validates :phone_number, presence: { message: '電話番号を入力してください' },
+                             format: { with: /\A\d{10,11}\z/, message: '電話番号は半角数字で入力してください' }
+    validates :token, presence: { message: 'カード情報が入力されていません' }
+    validates :user_id, presence: { message: 'ユーザー情報が不正です。' }
+    validates :item_id, presence: { message: '商品IDが不正です。' }
   end
 
   def save
